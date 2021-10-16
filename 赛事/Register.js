83b7dac2fb7dff9c8390fbcbb4ceb3dafc7f002a
博奕 黄 <1853308@tongji.edu.cn>
2021-10-16 11:55:46 +0800

    Update Register.js
    Encapsulate the Request api

--- 赛事/Register.js
+++ 赛事/Register.js
@@ -27,15 +27,41 @@ function token() {
 function baseURL() {
   return "https://gitee.com/api/v5/repos/AssistLGAG/yavatdata-repo/contents/championship/";
 }
-function getFileNameOnRemote(CommandType) { //CommandType为0时表示不需要上传文件，因此直接从输入框获取文件名
+function newRequest(method, url, body) {
+  if (method == "get") {
+    var _url = toString(url);
+    var isFirst = true;
+    for (const key in body) {
+      const value = body[key];
+      _url += isFirst ? "?" : "&";
+      isFirst = false;
+      _url += toString(key) + "=" + toString(value);
+    }
+    return new Request(_url, { method: method });
+  } else {
+    return new Request(url, {
+      method: method,
+      headers: headers(),
+      body: JSON.stringify(body),
+    });
+  }
+}
+
+function getFileNameOnRemote(CommandType) {
+  //CommandType为0时表示不需要上传文件，因此直接从输入框获取文件名
   const group = document.getElementById("groupName").value;
-  if(CommandType == 0) return group;
+  if (CommandType == 0) return group;
   else return group + getFileType();
 }
-function getFileType(){
+function getFileType() {
   var filename = document.getElementById("fileToBeUploaded").value;
-  var fileType = filename.substring(filename.lastIndexOf('.'));
-  if(fileType != ".doc" && fileType != ".docx" && fileType != ".xls" && fileType != ".xlsx"){
+  var fileType = filename.substring(filename.lastIndexOf("."));
+  if (
+    fileType != ".doc" &&
+    fileType != ".docx" &&
+    fileType != ".xls" &&
+    fileType != ".xlsx"
+  ) {
     alert("只接受word或者excel文件");
     return ".";
   }
@@ -43,10 +69,9 @@ function getFileType(){
 }
 async function requestFile(fileNameOnRemote) {
   if (!fileNameOnRemote) throw fileNameOnRemote;
-  const requestToGetSha = new Request(
-    baseURL() + fileNameOnRemote + "?access_token=" + token(),
-    { method: "get" }
-  );
+  const requestToGetSha = newRequest("get", baseURL() + fileNameOnRemote, {
+    access_token: token(),
+  });
   return await fetch(requestToGetSha);
 }
 async function requestFileSha(fileNameOnRemote) {
@@ -86,14 +111,10 @@ async function upload() {
   const fileContent = await getFileContent();
   const fileNameOnRemote = getFileNameOnRemote(1);
 
-  const request = new Request(baseURL() + fileNameOnRemote, {
-    method: "post",
-    headers: headers(),
-    body: JSON.stringify({
-      access_token: token(),
-      content: fileContent,
-      message: "Create " + fileNameOnRemote,
-    }),
+  const request = newRequest("post", baseURL() + fileNameOnRemote, {
+    access_token: token(),
+    content: fileContent,
+    message: "Create " + fileNameOnRemote,
   });
   const response = await fetch(request);
   if (!response.ok) alert("上传失败！原因为：" + (await response.text()));
@@ -105,15 +126,11 @@ async function update() {
 
   const fileContentToBeUploaded = await getFileContent();
 
-  const request = new Request(baseURL() + fileNameOnRemote, {
-    method: "put",
-    headers: headers(),
-    body: JSON.stringify({
-      access_token: token(),
-      sha: sha,
-      content: fileContentToBeUploaded,
-      message: "Update " + fileNameOnRemote,
-    }),
+  const request = newRequest("put", baseURL() + fileNameOnRemote, {
+    access_token: token(),
+    sha: sha,
+    content: fileContentToBeUploaded,
+    message: "Update " + fileNameOnRemote,
   });
   const response = await fetch(request);
   if (!response.ok) alert("更新失败！原因为：" + (await response.text()));
@@ -136,14 +153,10 @@ async function remove() {
   const fileNameOnRemote = getFileNameOnRemote(0);
   const sha = await requestFileSha(fileNameOnRemote);
 
-  const request = new Request(baseURL() + fileNameOnRemote, {
-    method: "delete",
-    headers: headers(),
-    body: JSON.stringify({
-      access_token: token(),
-      sha: sha,
-      message: "Delete " + fileNameOnRemote,
-    }),
+  const request = newRequest("delete", baseURL() + fileNameOnRemote, {
+    access_token: token(),
+    sha: sha,
+    message: "Delete " + fileNameOnRemote,
   });
   const response = await fetch(request);
   if (!response.ok) alert("删除失败！原因为：" + (await response.text()));
